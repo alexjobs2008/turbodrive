c2509038e83b12496a33ea68b34a0c309112d18a
Denis S. Tereshchenko <denis.s.tereshchenko@gmail.com>
2014-03-28 10:24:38 +0400

    ADDED: translations file

---
+++ src/translations.ts
@@ -0,0 +1,457 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Drive::AboutWidget</name>
+    <message>
+        <location filename="SettingsUI/AboutWidget.cpp" line="36"/>
+        <source>Homepage:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::AccountWidget</name>
+    <message>
+        <location filename="SettingsUI/AccountWidget.cpp" line="16"/>
+        <source>Logged in as:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AccountWidget.cpp" line="26"/>
+        <source>Reset Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AccountWidget.cpp" line="27"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AccountWidget.cpp" line="48"/>
+        <source>Open %1 folder</source>
+        <comment>%1 - product name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::AdvancedWidget</name>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="31"/>
+        <source>Show desktop notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="41"/>
+        <source>Selective Sync:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="46"/>
+        <source>Select which folders and files to sync...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="61"/>
+        <source>%1 folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="68"/>
+        <source>Move...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="82"/>
+        <source>Start % on system startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="91"/>
+        <source>Language:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/AdvancedWidget.cpp" line="152"/>
+        <source>Choose Folder to Move %1 Into</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::AppController</name>
+    <message>
+        <location filename="Application/AppController.cpp" line="111"/>
+        <source>Open Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/AppController.cpp" line="115"/>
+        <source>Pause Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/AppController.cpp" line="121"/>
+        <source>Resume Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/AppController.cpp" line="125"/>
+        <source>Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/AppController.cpp" line="129"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/AppController.cpp" line="171"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::AuthRestResource</name>
+    <message>
+        <location filename="APIClient/AuthenticationService.cpp" line="52"/>
+        <source>Please check your Internet connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="APIClient/AuthenticationService.cpp" line="68"/>
+        <source>No authentication token found</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::ConnectionWidget</name>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="39"/>
+        <source>Download rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="42"/>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="72"/>
+        <source>Don&apos;t limit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="48"/>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="77"/>
+        <source>Limit to:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="54"/>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="83"/>
+        <source>kB/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="69"/>
+        <source>Upload rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="106"/>
+        <source>Proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="111"/>
+        <source>No proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="114"/>
+        <source>Auto-detect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="119"/>
+        <source>Manual settings:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="121"/>
+        <source>Configure...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::FileDownloader</name>
+    <message>
+        <location filename="APIClient/FileDownloader.cpp" line="35"/>
+        <source>Failed to write to file: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="APIClient/FileDownloader.cpp" line="131"/>
+        <source>Network error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::FileSystemHelper</name>
+    <message>
+        <location filename="Util/FileUtils.cpp" line="55"/>
+        <source>[.ShellClassInfo]InfoTip=%1
+IconFile=%2
+IconIndex=%3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Util/FileUtils.cpp" line="60"/>
+        <source>%1 Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::LoginController</name>
+    <message>
+        <location filename="Application/LoginController.cpp" line="178"/>
+        <source>Please check your email</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/LoginController.cpp" line="179"/>
+        <source>We&apos;ve sent you an email with further instructions about password reset.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/LoginController.cpp" line="190"/>
+        <source>Password reset failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/LoginController.cpp" line="245"/>
+        <source>Login failed: can&apos;t obtain user details</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::LoginWidget</name>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="63"/>
+        <source>%1 Sign In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="93"/>
+        <source>Please wait...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="100"/>
+        <source>&amp;Phone:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="111"/>
+        <source>&amp;Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="122"/>
+        <source>Forgot?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="129"/>
+        <source>&amp;Sign me in when %1 starts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="139"/>
+        <source>Sign in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="145"/>
+        <source>or</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="148"/>
+        <source>Create account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="244"/>
+        <source>Please provide a valid phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/LoginWidget.cpp" line="251"/>
+        <source>Please provide a non-empty password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::PasswordResetResource</name>
+    <message>
+        <location filename="APIClient/DashboardService.cpp" line="114"/>
+        <source>Password reset failed: too many attempts, please try again later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="APIClient/DashboardService.cpp" line="117"/>
+        <source>Password reset failed: user not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="APIClient/DashboardService.cpp" line="121"/>
+        <source>Password reset failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::ProxySettingsDialog</name>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="238"/>
+        <source>Proxy Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="246"/>
+        <source>Proxy type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="283"/>
+        <source>Proxy server requires a password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="294"/>
+        <source>Username:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/ConnectionWidget.cpp" line="307"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::RemoteFileUploadedEventHandler</name>
+    <message>
+        <location filename="Events/EventHandlers.cpp" line="360"/>
+        <location filename="Events/RemoteEventHandlers.cpp" line="418"/>
+        <source>File uploaded event handler failed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::SelectiveSyncDialog</name>
+    <message>
+        <location filename="SettingsUI/SelectiveSyncDialog.cpp" line="28"/>
+        <source>Selective Sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SelectiveSyncDialog.cpp" line="35"/>
+        <source>Select which files and folder to sync:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::SettingsWidget</name>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="110"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="117"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="119"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="121"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="176"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="178"/>
+        <source>Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="180"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SettingsUI/SettingsWidget.cpp" line="182"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Drive::TrayIcon</name>
+    <message>
+        <location filename="Application/TrayIcon.cpp" line="30"/>
+        <source>%1 (%2 of %3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstSettingsWidget</name>
+    <message>
+        <location filename="LoginUI/FirstSettingsWidget.cpp" line="10"/>
+        <source>Typical settings (recommended)
+Set up Drive with normal settings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="LoginUI/FirstSettingsWidget.cpp" line="17"/>
+        <source>Advanced settings
+Choose Drive folder location and which folders and files to sync.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="Application/types.h" line="22"/>
+        <source>Not authorized</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/types.h" line="23"/>
+        <source>Signing in...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/types.h" line="24"/>
+        <source>Syncing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/types.h" line="25"/>
+        <source>Up to date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/types.h" line="26"/>
+        <source>Syncing paused</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Application/types.h" line="27"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
