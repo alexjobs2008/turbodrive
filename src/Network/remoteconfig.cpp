e35799f85d96e7a8045e555f2ab3387344aa67ad
Denis S. Tereshchenko <denis.s.tereshchenko@gmail.com>
2014-05-28 07:42:00 +0400

    IMPLEMENTED: remote config parsing

--- src/Network/remoteconfig.cpp
+++ src/Network/remoteconfig.cpp
@@ -3,7 +3,6 @@
 #include "SimpleDownloader.h"
 #include "RestService.h"
 
-#include <QsLog/QsLog.h>
 #include <QtCore>
 
 namespace Drive
@@ -17,69 +16,10 @@ RemoteConfig::RemoteConfig(const QString& url, QObject* parent)
 			this, &RemoteConfig::onDownloadFinished);
 }
 
-//{
-//	data: {
-//		update: {
-//			version: "1.0.0.0",
-//			url: "http://disk.mts.by/applications/MTS_Drive.exe"
-//		}
-//		environment: {
-//			name: "MTS",
-//			services: {
-//				"AuthService": "http://disk.mts.by",
-//				"DashboardService": "http://disk.mts.by",
-//				"ProfileService": "http://disk.mts.by",
-//				"FilesService": "http://disk.mts.by",
-//				"ContentService": "http://disk.mts.by",
-//				"SharingService": "http://disk.mts.by",
-//				"NotificationService": "http://rpl.disk.mts.by"
-//			}
-//		}
-//	}
-//}
-
-void RemoteConfig::onDownloadFinished(const QByteArray&)
+void RemoteConfig::onDownloadFinished(const QByteArray& data)
 {
-	QJsonObject json_services;
-	json_services.insert(QLatin1String("AuthService"), QLatin1String("http://disk.mts.by"));
-	json_services.insert(QLatin1String("DashboardService"), QLatin1String("http://disk.mts.by"));
-	json_services.insert(QLatin1String("ProfileService"), QLatin1String("http://disk.mts.by"));
-	json_services.insert(QLatin1String("FilesService"), QLatin1String("http://disk.mts.by"));
-	json_services.insert(QLatin1String("ContentService"), QLatin1String("http://disk.mts.by"));
-	json_services.insert(QLatin1String("SharingService"), QLatin1String("http://disk.mts.by"));
-	json_services.insert(QLatin1String("NotificationService"), QLatin1String("http://rpl.disk.mts.by"));
-
-	QJsonObject json_environment;
-	json_environment.insert(QLatin1String("name"), QLatin1String("MTS"));
-	json_environment.insert(QLatin1String("services"), json_services);
-
-	QJsonObject json_update;
-	json_update.insert(QLatin1String("version"), QLatin1String("1.0.0.0"));
-	json_update.insert(QLatin1String("url"), QLatin1String("http://disk.mts.by/applications/MTS_Drive.exe"));
-
-	QJsonObject json_data;
-	json_data.insert(QLatin1String("update"), json_update);
-	json_data.insert(QLatin1String("environment"), json_environment);
-
-	QJsonObject json_main;
-	json_main.insert(QLatin1String("data"), json_data);
-
-	const QByteArray d =
-			QJsonDocument(json_main).toJson(QJsonDocument::Compact);
-	QLOG_DEBUG() << d;
-
-
-
-
-
-
-
-
-
-
 	QJsonParseError error;
 
-	const QByteArray data = d;
 	auto doc = QJsonDocument::fromJson(data, &error);
 	Q_ASSERT(error.error == QJsonParseError::NoError);
 
