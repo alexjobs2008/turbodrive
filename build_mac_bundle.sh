21a9a8850ea4e22188694a6bea144673a924f435
alexjobs2008 <alex.jobs.2008@gmail.com>
2014-09-02 05:14:25 +0400

    5-14

--- build_mac_bundle.sh
+++ build_mac_bundle.sh
@@ -6,7 +6,7 @@ cd src-build
 # QTDIR=~/Qt/5.3/clang_64 
 echo $QTDIR
 
-cmake -DCMAKE_BUILD_TYPE=Release ../src -D "Unix Makefiles"
+cmake -DCMAKE_BUILD_TYPE=Release ../src -D"Unix Makefiles"
 make install
 
 $QTDIR/bin/macdeployqt ./Application/drive.app
